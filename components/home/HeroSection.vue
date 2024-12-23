4c26ac85f72f5ee1c99c6e4049b175c1ea3f115b
Pranav-2812 <pranavdhumale2812@gmail.com>
2024-12-23 12:22:28 +0530

    Name Issue

--- components/home/HeroSection.vue
+++ components/home/HeroSection.vue
@@ -2,12 +2,16 @@
   <v-container fluid class="">
     <v-row justify-center align="center">
       <v-col md="6" sm="6" cols="12">
-        <h1 class="responsive-h1 my-4">
-          <p class="d-flex flex-row">GDG <h6 style="font-size:medium; color:cornflowerblue;" class="mt-2 ms-2">On-Campus</h6></p> 
-          SGGS-Nanded 
+        <h1 class="responsive-h1 my-4" style="overflow-wrap: break-word;">
+          <p class="d-flex">
+            GDG
+          <h6 style="font-size: medium; color: cornflowerblue;" class="mt-2 ms-2">On-Campus</h6>
+          </p>
+          SGGS-Nanded
         </h1>
+
         <p class="" :style="{ maxWidth: '90%' }">
-          {{ mainData.communityDescription}}
+          {{ mainData.communityDescription }}
         </p>
 
         <p class="my-5">
@@ -17,33 +21,19 @@
             <a :href="mainData.eventInfo.venue.map_link" target="_blank" style="color:black">
               {{ mainData.eventInfo.venue.address }}
             </a>
-            
+
           </span>
         </p>
 
-        <v-btn
-          rounded
-          size="large"
-          color="#FFD427"
-          v-if="
-            mainData.eventInfo &&
-            mainData.eventInfo.registeration.link.length &&
-            new Date(mainData.eventInfo.registeration.end_date) > new Date()
-          "
-          :href="mainData.eventInfo.registeration.link"
-          class="my-4 mt-3"
-          target="_blank"
-          style="border: 1.5px solid #1e1e1e; color: black"
-          variant="flat"
-          >Register Now</v-btn
-        >
+        <v-btn rounded size="large" color="#FFD427" v-if="
+          mainData.eventInfo &&
+          mainData.eventInfo.registeration.link.length &&
+          new Date(mainData.eventInfo.registeration.end_date) > new Date()
+        " :href="mainData.eventInfo.registeration.link" class="my-4 mt-3" target="_blank"
+          style="border: 1.5px solid #1e1e1e; color: black" variant="flat">Register Now</v-btn>
       </v-col>
       <v-col md="6" sm="6" cols="12">
-        <v-img
-          alt="logo"
-          src="assets/img/hero-image.svg"
-          lazy-src="assets/img/hero-image.svg"
-        ></v-img>
+        <v-img alt="logo" src="assets/img/hero-image.svg" lazy-src="assets/img/hero-image.svg"></v-img>
       </v-col>
     </v-row>
   </v-container>
